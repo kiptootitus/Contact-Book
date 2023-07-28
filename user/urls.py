38e6687c263b8ca3f06c4e4d8ee70b54aaf4f599
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-07-28 13:16:14 +0300

    views

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.home, name ='home'),
+	path('login/', views.Login, name ='login'),
+	path('register/', views.register, name ='register'),
+
 ]
